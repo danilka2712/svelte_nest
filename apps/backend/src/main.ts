364a6b9c48d8317cc02352a63075babaf18b95d1
unknown <danilka2712@yandex.ru>
2023-01-16 12:51:14 +0600

    first commit

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,17 +1,11 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
  
 }
 
